ef6a1b81fbce5403a1cc0646408430088b0aea13
alexa-job <andreq_2008@list.ru>
2026-05-09 20:07:56 +0800

    Исправление наименования словаря

--- phonebook/phonebook.py
+++ phonebook/phonebook.py
@@ -1,46 +1,45 @@
 import json
 import os
 
-if os.path.exists("notebook.json"):
-    with open ("notebook.json", 'r', encoding='utf-8') as f:
-        notebook=json.load(f)  # загрузить
+if os.path.exists("phonebook.json"):
+    with open ("phonebook.json", 'r', encoding='utf-8') as f:
+        phonebook=json.load(f)  # загрузить
 else:
-    notebook = {}# пустой словарь
-
+    phonebook = {}# пустой словарь
 
 while True:
      com=input ("введите команду (добавить, список, поиск, удалить, выход): ").strip()
      if com.lower()=="выход":
-         with open ("notebook.json", 'w', encoding='utf-8') as f:
-          json.dump(notebook, f, ensure_ascii=False, indent=4)  
+         with open ("phonebook.json", 'w', encoding='utf-8') as f:
+          json.dump(phonebook, f, ensure_ascii=False, indent=4)  
          break
      elif com.lower()=="добавить":
          name_inp = input ("Введите имя контакта: ").strip()
          phone_inp = input ("Введите телефонный номер: ").strip()
-         if name_inp not in notebook:
-            notebook[name_inp]=phone_inp
+         if name_inp not in phonebook:
+            phonebook[name_inp]=phone_inp
             print (f"Имя: {name_inp}")
             print (f"Номер: {phone_inp}")
             print ("Контакт добавлен!")
-            with open ("notebook.json", 'w', encoding='utf-8') as f:
-                json.dump(notebook, f, ensure_ascii=False, indent=4)  
+            with open ("phonebook.json", 'w', encoding='utf-8') as f:
+                json.dump(phonebook, f, ensure_ascii=False, indent=4)  
          else:
             print ("Контакт был записан ранее!")
   
      elif com.lower()=="список": 
-            for name, phone in notebook.items():
+            for name, phone in phonebook.items():
              print(f"{name}: {phone}")
      elif com.lower()=="поиск":
             name_inp = input ("Введите имя контакта: ").strip()
-            for name, phone in notebook.items():
+            for name, phone in phonebook.items():
              if name_inp in name:
                print (f"Поиск по имени {name}: {phone}")
      elif com.lower() == "удалить":
           name_inp = input ("Введите имя контакта: ").strip()
-          if name_inp in notebook:
-            del notebook[name_inp]
+          if name_inp in phonebook:
+            del phonebook[name_inp]
             print ("Контакт удален!")
-            with open ("notebook.json", 'w', encoding='utf-8') as f:
-                json.dump(notebook, f, ensure_ascii=False, indent=4)  
+            with open ("phonebook.json", 'w', encoding='utf-8') as f:
+                json.dump(phonebook, f, ensure_ascii=False, indent=4)  
           else:
             print ("Контакт не найден!")

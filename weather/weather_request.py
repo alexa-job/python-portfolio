dc60e40501ab868784e3d1180d8a64273219f084
Андрей <andreq_2008@list.ru>
2026-05-14 11:39:17 +0800

    убрал: json, добавил: sql, вывод истории

--- weather/weather_request.py
+++ weather/weather_request.py
@@ -1,22 +1,36 @@
 import requests
-from json import dump, load
+import sqlite3
 from datetime import datetime
 import os
 
-if os.path.exists('meteoreguest.json'):
-    with open("meteoreguest.json", 'r', encoding='utf-8') as f:
-        data_meteo = load(f)
-else:
-    data_meteo = {}
+conn = sqlite3.connect("weater.db")
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS weather (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  city TEXT NOT NULL,
+                  temperature_max REAL,
+                  temperature_mim REAL,
+                  precipitation REAL,
+                  date TEXT NOT NULL)""")
     
-#inp_meteo = input ("Запрос (прогноз, история, обновить, выход)")
+inp_meteo = input ("Запрос (история, добавить): ")
+if inp_meteo.lower() == "история":
+   cursor.execute("SELECT * FROM weather ORDER BY city")
+   rows = cursor.fetchall()
+   if rows:
+      for row in rows:
+          print(f"{row[5]} - {row[1]}, макс: {row[2]}°C, мин: {row[3]}°C, осадки {row[4]} мм")
+   else:
+      print("История пуста!")
+   conn.close() 
+   exit()  
 city = input("Введите город: ")
 days = int(input("Период прогноза, дни: "))
 # Поиск координат
 geo_url = "https://geocoding-api.open-meteo.com/v1/search"
 geo_params = {"name": city, "count": 1, "language": "ru"}
 geo_data = requests.get(geo_url, params=geo_params).json()
-
 if "results" not in geo_data:
     print("Город не найден")
     exit()
@@ -47,30 +61,19 @@ print(f"\nСейчас: {weather['temperature']}°C, ветер {weather['windsp
 # Прогноз по дням
 print("\nПрогноз погоды:")
 daily = data["daily"]
-if city not in data_meteo:
-    data_meteo[city] = []
 time_current = datetime.now().strftime('%d.%m.%Y %H:%M')
 for i in range(days):
-       found = False
-       for record in data_meteo[city]:
-           if record['date'] == daily['time'][i]:
-                 record['time_request'] = time_current
-                 record["tmax"] = daily["temperature_2m_max"][i]
-                 record["tmin"] = daily["temperature_2m_min"][i]
-                 record["precipitation"] = daily["precipitation_sum"][i]
-                 found = True
-                 break
-       if not found:
-           data_meteo[city].append({ "time_request": time_current,
-                                     "date": daily["time"][i],
-                                     "tmax": daily["temperature_2m_max"][i],
-                                     "tmin": daily["temperature_2m_min"][i],
-                                     "precipitation": daily["precipitation_sum"][i]})
-          
-# if cur_data:
-#    data_meteo[city].append(cur_data)
-       
-print (data_meteo)
-
-with open("meteoreguest.json", 'w', encoding='utf-8') as f:
-     dump(data_meteo, f, ensure_ascii=False, indent=4)
+       # Чтение данных
+     found = False
+     cursor.execute("SELECT * FROM weather WHERE city = ? AND date = ?", (city, daily['time'][i]))
+     if cursor.fetchone():
+            cursor.execute("UPDATE weather SET temperature_max = ?, temperature_mim = ?, precipitation = ? WHERE city = ? AND date = ?",
+            (daily["temperature_2m_max"][i], daily["temperature_2m_min"][i], daily["precipitation_sum"][i], city, daily['time'][i]))
+            found = True
+     
+     if not found:   
+            cursor.execute("INSERT INTO weather (city, temperature_max, temperature_mim, precipitation, date) VALUES (?, ?, ?, ?, ?)",
+                           (city, daily["temperature_2m_max"][i], daily["temperature_2m_min"][i], daily["precipitation_sum"][i],daily['time'][i]))                
+    
+conn.commit()
+conn.close() 
